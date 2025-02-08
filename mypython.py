3563510e400f87342f0304774c65d00fbfeba77b
Jann Alexcis Mercado <jannmercado07@gmail.com>
2025-02-08 22:51:59 +0800

    practice

--- mypython.py
+++ mypython.py
@@ -48,6 +48,7 @@ while True:
 '''
 
 
+'''
 def check_balance(balance):
     print (f"Your balance is Php. {balance}")
 
@@ -74,22 +75,85 @@ while True:
     print("Press 2 for Deposit")
     print("Press 3 for Withdraw")
     print("Press 4 to Exit\n")
+    try:
+        user_input = int(input("Please Enter Any: "))
 
-    user_input = int(input("Please Enter Any: "))
+        if user_input == 1:
+            check_balance(balance)
 
-    if user_input == 1:
-        check_balance(balance)
+        elif user_input == 2:
+            balance = deposit(balance)
 
-    elif user_input == 2:
-        balance = deposit(balance)
+        elif user_input == 3:
+            balance = withdraw(balance)
 
-    elif user_input == 3:
-        balance = withdraw(balance)
+        elif user_input == 4:
+            break
 
-    elif user_input == 4:
-        break
+        else:
+            print ("Invalid Input")
 
-    else:
-        print ("Invalid Input")
+    except ValueError:
+        print("Please Enter an Integer")
 
 print("Thank you for using this.")
+'''
+
+
+
+
+def view_students (students):
+    if not students:
+        print ("No Inputs")
+    else:
+        for name, grade in students:
+            print(f"{name} {grade}")
+
+def add_students (students):
+    add_student = input ("Enter Student Name: ")
+    grade = float(input("Enter Student Grade: "))
+    students.append((add_student, grade))
+
+def update_grade (students):
+    view_students(students)
+    student_name = input("Enter Student Name: ")
+    new_grade = int(input("Enter new Grade: "))
+    for student in students:
+        student_index = students.index(student)
+
+    students[student_index] = student_name, new_grade
+    return students
+
+def remove_student (students):
+    view_students(students)
+    student_name = input("Enter Student Name: ")
+    for student in students:
+        if student[0] == student_name:
+            students.remove(student)
+    return students
+
+students = []
+while True:
+    
+    print ("\n1: view student: ")
+    print ("2: add student: ")
+    print ("3: update grade: ")
+    print ("4: remove student: ")
+    print ("5: quit\n")
+    
+#    try:
+    choice = int(input ("Enter Choice: "))
+    if choice == 1:
+        view_students(students)
+    elif choice == 2:
+        add_students(students)
+    elif choice == 3:
+        update_grade(students)
+    elif choice == 4:
+        remove_student(students)
+    elif choice == 5:
+        break
+    else:
+        print ("Invalid Input")
+ #   except ValueError:
+ #       print ("Please Input Integers Only")
