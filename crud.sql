0e67c59db2227556b909053020037c8f52ebba67
Jann Alexcis Mercado <jannmercado07@gmail.com>
2025-02-11 23:16:04 +0800

    CRUD

---
+++ crud.sql
@@ -0,0 +1,29 @@
+psql -U postgres
+create schema qrio;
+    ALTER SCHEMA qrio OWNER TO jann;
+CREATE USER jann WITH SUPERUSER PASSWORD 'admin';
+
+drop table person;
+
+create table person (
+	id SERIAL primary key,
+	firstname VARCHAR(100) not null,
+	lastname VARCHAR(100) not null,
+	age INT,
+	active bool default true
+)
+
+--CREATE
+insert into person (firstname, lastname, age)
+values ('Jann','Mercado', 22);
+
+--READ
+select * from person;
+
+--UPDATE
+update person
+set age =23
+where id = 1;
+
+--DELETE
+delete from person where  AGE >= 23;
